d7d1856b82eefe8a4f526db066e06ddc7303a5d5
fieldstyler <tdfields93@gmail.com>
2021-04-29 10:24:46 -0600

    Create Welcome Page and Controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+album = Album.create(title: "Black Hole - EP", genre: "awesome", release_date: "2021", artist: "mofie")
+Song.create(title: "Swing & a Miss", length: 267, album: album)
+Song.create(title: "Yoursnotmine", length: 207, album: album)
+Song.create(title: "Stranger Now", length: 118, album: album)
+Song.create(title: "The Flower Bed", length: 271, album: album)
+Song.create(title: "Black Hole", length: 206, album: album)
