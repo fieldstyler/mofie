3ca41ae64a2813dfdcd851d13e91bfecd8ce2043
fieldstyler <tdfields93@gmail.com>
2021-04-30 14:05:38 -0600

    Add clickable links to welcome page to access albums and songs

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -5,4 +5,30 @@ RSpec.describe "Welcome Page" do
     visit root_path
     expect(page).to have_content("mofie")
   end
+
+  it "has clickable link to albums" do
+    album = Album.create(title: "Black Hole - EP", genre: "awesome", release_date: "2021", artist: "mofie")
+    Song.create(title: "Swing & a Miss", length: 267, album: album)
+    Song.create(title: "Yoursnotmine", length: 207, album: album)
+    Song.create(title: "Stranger Now", length: 118, album: album)
+    Song.create(title: "The Flower Bed", length: 271, album: album)
+    Song.create(title: "Black Hole", length: 206, album: album)
+    visit root_path
+    expect(page).to have_content("Albums")
+    click_on "Albums"
+    expect(current_path).to eq("/albums")
+  end
+
+  it "has clickable link to songs" do
+    album = Album.create(title: "Black Hole - EP", genre: "awesome", release_date: "2021", artist: "mofie")
+    Song.create(title: "Swing & a Miss", length: 267, album: album)
+    Song.create(title: "Yoursnotmine", length: 207, album: album)
+    Song.create(title: "Stranger Now", length: 118, album: album)
+    Song.create(title: "The Flower Bed", length: 271, album: album)
+    Song.create(title: "Black Hole", length: 206, album: album)
+    visit root_path
+    expect(page).to have_content("Songs")
+    click_on "Songs"
+    expect(current_path).to eq("/songs")
+  end
 end
